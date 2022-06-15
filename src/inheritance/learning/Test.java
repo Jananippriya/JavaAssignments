620f592c9f9698de0719e87ffbcd3819d464eb68
Jananippriya <janupriya27@gmail.com>
2022-06-15 15:34:15 -0400

    inheritance test class

--- src/inheritance/learning/Test.java
+++ src/inheritance/learning/Test.java
@@ -4,13 +4,13 @@ import java.util.Date;
 
 public class Test {
     public static void main(String[] args) {
-        Employee emp = new Employee("Janu",12345,"IT",new Date(2019,5,12),40);
+        Employee emp = new Employee("Janu",12345,"IT",new Date(119,5,12),40);
         System.out.println(emp.getEmployeeDetails());
 
-        Manager man = new Manager("Janani",123,"HR",new Date(2010,01,01),50,1000);
+        Manager man = new Manager("Janani",123,"HR",new Date(110,01,01),50,1000);
         System.out.println(man.getEmployeeDetails());
 
-        Director dir = new Director("Priya",1,"HR",new Date(2009,05,10),70,1500,1000);
+        Director dir = new Director("Priya",1,"HR",new Date(109,05,10),70,1500,1000);
         System.out.println(dir.getEmployeeDetails());
     }
 }
