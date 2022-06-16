1083d7d91d1a9e24d63de8bfb6ca3f2265e96444
Jananippriya <janupriya27@gmail.com>
2022-06-15 20:28:44 -0400

    Ex-3 Assignment solutions

---
+++ src/oops/concepts/learning/ex3/Oops2.java
@@ -0,0 +1,8 @@
+package oops.concepts.learning.ex3;
+
+public class Oops2 {
+    public static void main(String[] args) {
+        Circle1 c1 = new Circle1(5.5);
+        System.out.println("Area of the circle c1 is: "+c1.area());
+    }
+}
